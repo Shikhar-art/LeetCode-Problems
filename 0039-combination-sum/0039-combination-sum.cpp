1c6c351cf2f0edbb020b66c7557d0d15144e2308
Shikhar_art <shikharshourya975@gmail.com>
2026-07-20 20:53:17 +0530

    Time: 4 ms (37.78%), Space: 14 MB (93.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    
-   
 public:
- set<vector<int>>s;
-void get(vector<int>& arr,int idx,int tar,vector<vector<int>>&ans,vector<int> &combin){
-    if (tar == 0) {
+    void get(vector<int>& arr, int idx, int tar,
+             vector<vector<int>>& ans, vector<int>& combin) {
+
+        if (tar == 0) {
             ans.push_back(combin);
             return;
         }
-        if (idx >= arr.size() || tar < 0) return;
 
-        // include arr[idx]
+        if (idx >= arr.size() || tar < 0)
+            return;
+
         combin.push_back(arr[idx]);
-        get(arr, idx, tar - arr[idx], ans, combin); // can reuse same element
+        get(arr, idx, tar - arr[idx], ans, combin);
         combin.pop_back();
 
-        // move to next element
         get(arr, idx + 1, tar, ans, combin);
-}
+    }
+
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>>ans;
-        vector<int>combin;
-        get(arr,0,target,ans,combin);
+        vector<vector<int>> ans;
+        vector<int> combin;
+        get(arr, 0, target, ans, combin);
         return ans;
     }
 };
